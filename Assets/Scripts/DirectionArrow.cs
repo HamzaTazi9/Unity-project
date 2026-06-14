d9835180230daf59419018ff7ffd4bc5c03c0431
Zineb <m.zineb@Zinebs-MacBook-Pro.local>
2026-06-14 15:02:46 +0200

    Fix package pickup and direction arrow

--- Assets/Scripts/DirectionArrow.cs
+++ Assets/Scripts/DirectionArrow.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class DirectionArrow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform packageTarget;
+    public Transform deliveryTarget;
+    public Package packageScript;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Transform target = packageTarget;
+
+        if (packageScript != null && packageScript.IsPickedUp())
+        {
+            target = deliveryTarget;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        direction.y = 0;
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = targetRotation;
+        }
     }
 }
--- Assets/Scripts/Package.cs
+++ Assets/Scripts/Package.cs
@@ -4,12 +4,16 @@ public class Package : MonoBehaviour
 {
     public Transform packageHolder;
 
+    private bool isPickedUp = false;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger geraakt door: " + other.name);
 
         if (other.CompareTag("Player"))
         {
+            isPickedUp = true;
+
             transform.SetParent(packageHolder);
             transform.localPosition = Vector3.zero;
             transform.localRotation = Quaternion.identity;
@@ -17,4 +21,9 @@ public class Package : MonoBehaviour
             Debug.Log("Package picked up!");
         }
     }
+
+    public bool IsPickedUp()
+{
+    return isPickedUp;
+}
 }
