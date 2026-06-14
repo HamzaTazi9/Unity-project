a19f53bf4b52dc96edbcde5a73a341a1057149ad
r1050902 <r1050902@student.thomasmore.be>
2026-06-14 12:59:42 +0200

    Connect Game Over score and time UI
    Linked the Game Over score text and time text to the GameManager. The Game Over screen can now display the player’s final score and the time used when the game ends. Also prepared the restart button to reload the scene and allow the player to try again.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,48 +2,59 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-
 public class GameManager : MonoBehaviour
 {
     public GameObject startPanel;
     public GameObject gameOverPanel;
+    public GameObject winPanel;
 
-public TMP_Text packageCounterText;
+    public TMP_Text packageCounterText;
     public TMP_Text gameOverScoreText;
+    public TMP_Text gameOverTimeText;
+
+    public TMP_Text winScoreText;
+    public TMP_Text winTimeText;
 
     public int totalPackages = 3;
 
     private int deliveredPackages = 0;
 
     public TMP_Text timerText;
-public float timeLeft = 60f;
+    public float timeLeft = 60f;
+    private float startingTime;
 
-public TMP_Text scoreText;
+    public TMP_Text scoreText;
 
-public TMP_Text highScoreText;
+    public TMP_Text highScoreText;
+
     private bool gameStarted = false;
     private bool gameEnded = false;
 
     private int score = 0;
-public int pointsPerPackage = 100;
+    public int pointsPerPackage = 100;
 
-private int highScore = 0;
+    private int highScore = 0;
 
     void Start()
     {
         Time.timeScale = 0f;
 
+        startingTime = timeLeft;
+
         startPanel.SetActive(true);
         gameOverPanel.SetActive(false);
 
-         UpdatePackageCounter();
+        if (winPanel != null)
+        {
+            winPanel.SetActive(false);
+        }
 
-         UpdateTimerText();
+        UpdatePackageCounter();
+        UpdateTimerText();
+        UpdateScoreText();
 
-         UpdateScoreText();
-
-         highScore = PlayerPrefs.GetInt("HighScore", 0);
-UpdateHighScoreText();
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        UpdateHighScoreText();
     }
 
     public void StartGame()
@@ -55,58 +66,75 @@ UpdateHighScoreText();
 
     public void DeliverPackage()
     {
-         if (!gameStarted || gameEnded)
-    {
-        return;
-    }
+        if (!gameStarted || gameEnded)
+        {
+            return;
+        }
 
-    deliveredPackages++;
+        deliveredPackages++;
 
-    score += pointsPerPackage;
-UpdateScoreText();
+        score += pointsPerPackage;
+        UpdateScoreText();
 
-    UpdatePackageCounter();
+        UpdatePackageCounter();
 
-    Debug.Log("Package delivered: " + deliveredPackages + "/" + totalPackages);
+        Debug.Log("Package delivered: " + deliveredPackages + "/" + totalPackages);
+
+        if (deliveredPackages >= totalPackages)
+        {
+            WinGame();
+        }
     }
 
     void UpdatePackageCounter()
-{
-    packageCounterText.text = "Packages: " + deliveredPackages + "/" + totalPackages;
-}
-
-void Update()
-{
-    if (!gameStarted || gameEnded)
     {
-        return;
+        if (packageCounterText != null)
+        {
+            packageCounterText.text = "Packages: " + deliveredPackages + "/" + totalPackages;
+        }
     }
 
-    timeLeft -= Time.deltaTime;
-
-    if (timeLeft <= 0)
+    void Update()
     {
-        timeLeft = 0;
-        GameOver();
+        if (!gameStarted || gameEnded)
+        {
+            return;
+        }
+
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0)
+        {
+            timeLeft = 0;
+            GameOver();
+        }
+
+        UpdateTimerText();
     }
 
-    UpdateTimerText();
-}
+    void UpdateTimerText()
+    {
+        if (timerText != null)
+        {
+            timerText.text = "Time: " + Mathf.CeilToInt(timeLeft);
+        }
+    }
 
-void UpdateTimerText()
-{
-    timerText.text = "Time: " + Mathf.CeilToInt(timeLeft);
-}
+    void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
 
-void UpdateScoreText()
-{
-    scoreText.text = "Score: " + score;
-}
-
-void UpdateHighScoreText()
-{
-    highScoreText.text = "High Score: " + highScore;
-}
+    void UpdateHighScoreText()
+    {
+        if (highScoreText != null)
+        {
+            highScoreText.text = "High Score: " + highScore;
+        }
+    }
 
     public void GameOver()
     {
@@ -122,19 +150,69 @@ void UpdateHighScoreText()
 
         if (gameOverScoreText != null)
         {
-            gameOverScoreText.text = "Packages delivered: " + deliveredPackages + "/" + totalPackages;
+            gameOverScoreText.text = "Score: " + score;
+        }
+
+        if (gameOverTimeText != null)
+        {
+            float timeUsed = startingTime - timeLeft;
+            int minutes = Mathf.FloorToInt(timeUsed / 60f);
+            int seconds = Mathf.FloorToInt(timeUsed % 60f);
+
+            gameOverTimeText.text = "Time: " + minutes.ToString("00") + ":" + seconds.ToString("00");
         }
 
         if (score > highScore)
-{
-    highScore = score;
-    PlayerPrefs.SetInt("HighScore", highScore);
-    PlayerPrefs.Save();
-}
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+        }
 
-UpdateHighScoreText();
+        UpdateHighScoreText();
 
         Debug.Log("GAME OVER");
+    }
+
+    public void WinGame()
+    {
+        if (gameEnded)
+        {
+            return;
+        }
+
+        gameEnded = true;
+        Time.timeScale = 0f;
+
+        if (winPanel != null)
+        {
+            winPanel.SetActive(true);
+        }
+
+        if (winScoreText != null)
+        {
+            winScoreText.text = "Score: " + score;
+        }
+
+        if (winTimeText != null)
+        {
+            float timeUsed = startingTime - timeLeft;
+            int minutes = Mathf.FloorToInt(timeUsed / 60f);
+            int seconds = Mathf.FloorToInt(timeUsed % 60f);
+
+            winTimeText.text = "Time: " + minutes.ToString("00") + ":" + seconds.ToString("00");
+        }
+
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+        }
+
+        UpdateHighScoreText();
+
+        Debug.Log("YOU WIN");
     }
 
     public void RestartGame()
